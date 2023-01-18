6a601aa7c55bc6e59ef9c82d49eea146e0dce958
Vsvvn <Vladsun@yandex.ru>
2023-01-18 04:27:07 +0300

    Первый коммит.

---
+++ src/main/java/vs/recipes/HomeWork24Application.java
@@ -0,0 +1,13 @@
+package vs.recipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork24Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWork24Application.class, args);
+    }
+
+}
---
+++ src/test/java/vs/recipes/HomeWork24ApplicationTests.java
@@ -0,0 +1,13 @@
+package vs.recipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork24ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
